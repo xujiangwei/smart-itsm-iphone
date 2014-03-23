c4d2ec60932218b63dc6236cc9a63a6a3bd99634
Apple002 <zhuguoqiang_2010@163.com>
2014-03-23 18:38:26 +0800

    删除JSONKit库，添加告警控制器，修改登录

---
+++ SmartITSM/SmartITSM/SAlarmContentViewController.h
@@ -0,0 +1,13 @@
+//
+//  SAlarmContentViewController.h
+//  SmartITSM
+//
+//  Created by 朱国强 on 14-3-23.
+//  Copyright (c) 2014年 Ambrose. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SAlarmContentViewController : UIViewController
+
+@end
---
+++ SmartITSM/SmartITSM/SAlarmContentViewController.m
@@ -0,0 +1,38 @@
+//
+//  SAlarmContentViewController.m
+//  SmartITSM
+//
+//  Created by 朱国强 on 14-3-23.
+//  Copyright (c) 2014年 Ambrose. All rights reserved.
+//
+
+#import "SAlarmContentViewController.h"
+
+@interface SAlarmContentViewController ()
+
+@end
+
+@implementation SAlarmContentViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
