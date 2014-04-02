1e6ed1335f08cbdb18fe1b7dc231ae144d39afd3
qw1210p <qw1210p@gmail.com>
2014-04-02 11:22:01 +0800

    no message

---
+++ SmartITSM/SmartITSM/SProcessTemplate.h
@@ -0,0 +1,13 @@
+//
+//  SProcessTemplate.h
+//  SmartITSM
+//
+//  Created by dweng on 14-4-2.
+//  Copyright (c) 2014年 Ambrose. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SProcessTemplate : NSObject
+
+@end
---
+++ SmartITSM/SmartITSM/SProcessTemplate.m
@@ -0,0 +1,13 @@
+//
+//  SProcessTemplate.m
+//  SmartITSM
+//
+//  Created by dweng on 14-4-2.
+//  Copyright (c) 2014年 Ambrose. All rights reserved.
+//
+
+#import "SProcessTemplate.h"
+
+@implementation SProcessTemplate
+
+@end
